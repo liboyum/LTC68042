67252589a849d4dd72aa37783740391886f15169
liboyum <34636303+liboyum@users.noreply.github.com>
2018-08-09 11:24:30 -0400

    Update main.c

--- main.c
+++ main.c
@@ -33,14 +33,13 @@ int main(void)
 
 	printf("Raspberry Pi LTC6804-2 voltage test program");
 	if(wiringPiSetup() == -1)
-		exit(1);
-	while(1);
 	{
-		LTC68042_Broadcast_ADCV();
-		delay(500);
-		LTC68042_Address_RDCVA();
-		delay(500);
+		exit(1);
 	}
+	LTC68042_Broadcast_ADCV();
+	delay(500);
+	LTC68042_Address_RDCVA();
+	delay(500);
 	LTC68042_Broadcast_CLRCELL();
 	return 0;
 }
