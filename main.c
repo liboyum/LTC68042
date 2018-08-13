998140f6484839d85dc37bd04363cdcaaf4b30d4
liboyum <34636303+liboyum@users.noreply.github.com>
2018-08-13 15:00:01 -0400

    Update main.c

--- main.c
+++ main.c
@@ -161,9 +161,12 @@ int main(void)
         output_low(SCK);
         output_low(MOSI);
         output_high(LTC6804_CS);
+	while(1)
+	{
 	LTC6804_adcv();
 	LTC6804_rdcv(0, 1, cell_codes);
 	print_voltage(cell_codes);
+	}
 	return 0;
 }
 
