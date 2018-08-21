4edb99c49fbe73ab062ad67ee9c2bd77d13f5de8
liboyum <34636303+liboyum@users.noreply.github.com>
2018-08-21 10:39:45 -0400

    Update main.c

--- main.c
+++ main.c
@@ -9,12 +9,11 @@
 #define TOTAL_IC 1
 
 void print_voltage();
-//uint16_t cell_codes[TOTAL_IC][12];
+uint16_t cell_codes[TOTAL_IC][12];
 int rdError = 0;
 
 int main(void)
 {
-	uint16_t cell_codes[TOTAL_IC][12];
 	printf("Raspberry Pi LTC6804-2 voltage test program\n");
 	LTC6804_initialize();
         pinMode(SCK, OUTPUT);             //! 1) Setup SCK as output
@@ -26,18 +25,15 @@ int main(void)
 		printf("A PEC error was detected in the received data\n");
 	}
 	else{
-		//print_voltage();
-		 for(int i=0; i<12; i++){
-                     printf("The voltage is %.4f\n", cell_codes[TOTAL_IC][i]*0.0001);
-                 }
+		print_voltage();
 	}
 	return 0;
 }
 
-// void print_voltage()
-// {
-//     for(int i=0; i<12; i++)
-//     {
-//       printf("The voltage is %.2f\n", cell_codes[TOTAL_IC][i]*0.0001);
-//     }
-// }
+void print_voltage()
+{
+    for(int i=0; i<12; i++)
+    {
+       printf("The voltage is %.4f\n", cell_codes[TOTAL_IC][i]*0.0001);
+     }
+}
