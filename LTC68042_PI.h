db340efd7ea3ef7bd9e1a9e80ee61d56db5dd434
liboyum <34636303+liboyum@users.noreply.github.com>
2018-08-21 11:04:12 -0400

    Update LTC68042_PI.h

--- LTC68042_PI.h
+++ LTC68042_PI.h
@@ -2,6 +2,7 @@
 #define LTC68042_PI_H
 
 #include <wiringPi.h>
+#include <wiringPiSPI.h>
 
 #define output_low(pin)    digitalWrite(pin, LOW);
 #define output_high(pin)   digitalWrite(pin, HIGH);
@@ -14,4 +15,5 @@
 
 #define CHANNEL 0
 #define SPEED   500000
+
 #endif
